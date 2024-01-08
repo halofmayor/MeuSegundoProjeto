b32f977ebe193d87e5afb2021ebb01d75a142b83
halof <halofmayor@gmail.com>
2024-01-08 02:37:38 +0000

    Initial commit

---
+++ src/MeuSegundoPrograma.java
@@ -0,0 +1,14 @@
+public class MeuSegundoPrograma {
+    public static void main(String[] args) {
+        String login = "Olá";
+        int number1 = 15;
+        double number2 = 15.44;
+        boolean isDriver = false;
+        //tipo: Textos (Strings)
+        //tipo: numeros (Int)
+        //tipo: numeros fracionados (Double)
+        //tipo: booleanas (verdadeiro | falso)
+
+        System.out.println(login + number1 + number2 + isDriver);
+    }
+}
